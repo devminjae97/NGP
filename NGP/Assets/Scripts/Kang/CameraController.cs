9dcbda73e1e354a08c257df97a62a9464ace6982
강영규 <skj45691@gmail.com>
2024-04-19 02:44:20 +0900

    FIxed CharacterMovement & Camera Controller
    이상 버그들 수정.
    카메라가 플레이어 따라가는 것에 문제 발생 해결
    캐릭터가 제대로 못 움직이던 현상 해결
    변수 이름들 public 이외의 변수들을 '_'를 붙여서 구분

--- NGP/Assets/Scripts/Kang/CameraController.cs
+++ NGP/Assets/Scripts/Kang/CameraController.cs
@@ -3,44 +3,59 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5f; // 카메라 이동 속도
-    [SerializeField] float followDelay = 0.5f; // 딜레이 시간
-    [SerializeField] Transform target; // 플레이어의 Transform
-    [SerializeField] CharacterMovement characterMovement;
+    [SerializeField] protected float _cameraMoveSpeed = 5f;
+    [SerializeField] protected float _followDelay = 0.5f;
+    [SerializeField] protected Transform _target;
+    [SerializeField] protected CharacterMovement _characterMovement;
 
-    private Vector3 defaultCameraPosition = new Vector3(0.0f, 0.0f, -10f);
-    private Vector3 targetPosition; // 목표 위치
-    private Vector3 velocity = Vector3.zero; // 현재 속도
- 
-    private void Start()
+    private Vector3 _defaultCameraPosition = new Vector3(0.0f, 0.0f, -10f);
+    private Vector3 _targetPosition;
+    private Vector3 _velocity = Vector3.zero;
+
+    private void Awake()
     {
-        characterMovement = FindObjectOfType<CharacterMovement>();
-        transform.position = target.position + defaultCameraPosition;
+        _characterMovement = FindObjectOfType<CharacterMovement>();
+        if (_target != null)
+        {
+            transform.position = _target.position + _defaultCameraPosition;
+        }
     }
+
     private void Update()
     {
-        if (characterMovement.IsPlayerStopped())
+        if (_characterMovement.IsPlayerStopped() && IsUsing())
             MoveCameraWithInput();
         else
             FollowPlayer();
     }
 
+    bool IsUsing()
+    {
+        if (GetInput() != new Vector2(0f,0f))
+            return  true;
+        else
+            return false;
+       
+    }
+
     void FollowPlayer()
     {
-        if (target != null)
+        if (_target != null)
         {
             // 플레이어를 중심으로 카메라 이동
-            targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, followDelay);
+            _targetPosition = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref _velocity, _followDelay);
         }
     }
+
     void MoveCameraWithInput()
     {
         // WASD 입력을 받아 카메라 이동
         Vector2 moveInput = GetInput();
         Vector3 moveDirection = new Vector3(moveInput.x, moveInput.y, 0f).normalized;
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        transform.position += moveDirection * _cameraMoveSpeed * Time.deltaTime;
     }
+
     Vector2 GetInput()
     {
         Vector2 moveInput = Vector2.zero;
