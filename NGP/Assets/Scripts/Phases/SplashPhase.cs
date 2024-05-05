b724af80bab85e72e7b2861b9fdcf93087f83849
Minjae_Cho <t9100@naver.com>
2024-05-05 20:51:52 +0900

    [구현] Splash 임시구현

--- NGP/Assets/Scripts/Phases/SplashPhase.cs
+++ NGP/Assets/Scripts/Phases/SplashPhase.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class SplashPhase : PhaseBase
 {
-    // Start is called before the first frame update
+    [SerializeField] private Image _splashImage;
+
     protected override void Initialize()
     {
-        // @TODO
+        _splashImage.color = new (1,1,1,0);
     }
 
     protected override void StartPhase()
     {
-        // @TODO
+        if (_splashImage == null)
+        {
+            return;
+        }
+        
+        AsyncShowSplash().Forget();
     }
 
+    private async UniTaskVoid AsyncShowSplash()
+    {
+        await UniTask.Delay(1000, cancellationToken: this.GetCancellationTokenOnDestroy());
+        
+        // Appear Logo (Tween)
+        var appearTween = _splashImage.DOFade(1, 1);
+        await appearTween.Play();
+
+        // wait
+        await UniTask.Delay(2000, cancellationToken: this.GetCancellationTokenOnDestroy());
+
+        // Disappear Logo (Tween)
+        var disappearTween = _splashImage.DOFade(0, 1);
+        await disappearTween.Play();
+
+        await UniTask.Delay(500, cancellationToken: this.GetCancellationTokenOnDestroy());
+
+        SceneManager.LoadScene("LobbyScene");
+    }
 }
